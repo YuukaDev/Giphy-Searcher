b202887d6e23f0a0a2911f0479bdd374f837eb1c
YuukaDev <vule.amv1@gmail.com>
2021-11-28 20:56:13 +0100

    Create Search.js

---
+++ src/components/Input/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input } from '@chakra-ui/react';
+
+function Search() {
+  return (
+    <div className="form-container">
+      <Input placeholder="Enter name" w="20%" />
+    </div>
+  );
+}
+
+export default Search;
