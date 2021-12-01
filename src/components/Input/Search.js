f03a0f35f157c857d54e8d45dd44f76ee49a384c
YuukaDev <vule.amv1@gmail.com>
2021-12-01 22:10:06 +0100

    Update Search.js

--- src/components/Input/Search.js
+++ src/components/Input/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Input } from '@chakra-ui/react';
+import { Input, Stack } from '@chakra-ui/react';
 
 function Search() {
   return (
     <div className="form-container">
-      <Input placeholder="Enter name" w="100%" />
+      <Stack spacing={3}>
+        <Input p="20px"  placeholder="Enter name" w="100%" />
+      </Stack>
     </div>
   );
 }
