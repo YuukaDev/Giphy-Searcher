f723c1ed2152399a1c7ffe8f545055b39ed96ce4
YuukaDev <vule.amv1@gmail.com>
2021-12-01 23:05:39 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,8 @@
-import { Heading, Text, Box } from '@chakra-ui/react';
 import './Header.css';
 function Header() {
   return (
     <div className="wrapper">
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        color="white"
-      >
-        <Heading as="h3" className="header" size="xl">
-          GIPHY
-        </Heading>
-        <Text fontSize="2xl">
-          <a className="spacing" href="https://github.com/YuukaDev">
-            GitHub
-          </a>
-        </Text>
-        <Text fontSize="2xl">
-          <a className="spacing" href="https://twitter.com/yuukasuoh">
-            Twitter
-          </a>
-        </Text>
-      </Box>
+      <h1>Giphy Searcher</h1>
     </div>
   );
 }
