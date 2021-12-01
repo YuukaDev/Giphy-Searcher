d69c09d9e36604addd13cfe6c496d2a04b5cd8b6
YuukaDev <vule.amv1@gmail.com>
2021-12-01 23:05:35 +0100

    Create Content.js

---
+++ src/components/Content/Content.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Content.css';
+
+function Content() {
+  return (
+    <div className="container">
+      <form
+        className="form"
+        onSubmit={e => {
+          e.preventDefault();
+          const value = e.target.elements.input.value;
+          if (!value) {
+            alert('Enter gif name!');
+          } else {
+            alert(value);
+          }
+        }}
+      >
+        <div className="wrapper-form">
+          <input type="text" id="input" placeholder="Enter a username..." />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Content;
