1aaffe587908d3116f03169d5337a2a12a5a6332
YuukaDev <vule.amv1@gmail.com>
2021-12-02 21:07:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
-
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 
 function App() {
   return (
-    <ChakraProvider>
-      <Navigation />
+    <div>
       <Header />
       <Content />
-    </ChakraProvider>
+    </div>
   );
 }
 
